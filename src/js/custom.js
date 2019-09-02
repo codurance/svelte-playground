94381c6ecec079586114ec3181e33e53c62081e3
Damián Pumar <damianpumar@gmail.com>
2019-09-02 12:07:02 +0200

    Flip action to change view from original to demo

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,40 +1,61 @@
-$(document).ready(function () {
-    $('.navbar-toggler').on('click', function () {
-        $('.sidebar').toggleClass('active');
-    });
+$(document).ready(function() {
+  $(".navbar-toggler").on("click", function() {
+    $(".sidebar").toggleClass("active");
+  });
 
-    $('.social .fa').each(function () {
-        this.href+= document.location.href;
-    });
+  $(".social-links a").each(function() {
+    this.href += document.location.href;
+  });
+
+  $("#original").click(function() {
+    $("#body").flip('toggle');
+  });
+
+  $("#modificado").click(function() {
+    $("#body").flip('toggle');
+  });
+
+  $("#body").flip({
+    trigger: 'manual'
+  });
 });
 
-$(function(){
-    jQuery('img.svg').each(function(){
-        var $img = jQuery(this);
-        var imgID = $img.attr('id');
-        var imgClass = $img.attr('class');
-        var imgURL = $img.attr('src');
-    
-        $.get(imgURL, function(data) {
-            var $svg = jQuery(data).find('svg');
-    
-            if(typeof imgID !== 'undefined') {
-                $svg = $svg.attr('id', imgID);
-            }
+$(function() {
+  jQuery("img.svg").each(function() {
+    var $img = jQuery(this);
+    var imgID = $img.attr("id");
+    var imgClass = $img.attr("class");
+    var imgURL = $img.attr("src");
 
-            if(typeof imgClass !== 'undefined') {
-                $svg = $svg.attr('class', imgClass+' replaced-svg');
-            }
-    
-            $svg = $svg.removeAttr('xmlns:a');
-            
-            if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-                $svg.attr('viewBox', '0 0 ' + $svg.attr('width') + ' ' + $svg.attr('height'))
-            }
-    
-            $img.replaceWith($svg);
-    
-        }, 'xml');
-    
-    });
+    $.get(
+      imgURL,
+      function(data) {
+        var $svg = jQuery(data).find("svg");
+
+        if (typeof imgID !== "undefined") {
+          $svg = $svg.attr("id", imgID);
+        }
+
+        if (typeof imgClass !== "undefined") {
+          $svg = $svg.attr("class", imgClass + " replaced-svg");
+        }
+
+        $svg = $svg.removeAttr("xmlns:a");
+
+        if (
+          !$svg.attr("viewBox") &&
+          $svg.attr("height") &&
+          $svg.attr("width")
+        ) {
+          $svg.attr(
+            "viewBox",
+            "0 0 " + $svg.attr("width") + " " + $svg.attr("height")
+          );
+        }
+
+        $img.replaceWith($svg);
+      },
+      "xml"
+    );
+  });
 });
