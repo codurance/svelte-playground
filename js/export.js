89b7990591b9a39a08981f095a7873c3df5cefed
Damián Pumar <damianpumar@gmail.com>
2019-09-05 11:59:23 +0200

    New release

--- js/export.js
+++ js/export.js
@@ -1,17 +1,1 @@
-'use strict';
-
-function printMap() {
-  var map = document.querySelector("#abs-bcn-map");
-
-  html2canvas(map).then(canvas => {
-    let pdf = new jsPDF("p", "mm", "a4");
-    pdf.addImage(canvas.toDataURL(), "PNG", 0, 0, 211, 298);
-    pdf.save(map.dataset.filename);
-  });
-}
-
-$(document).ready(function () {
-  $("#exportMap").on("click", function () {
-    printMap();
-  });
-});
+"use strict";function printMap(){var a=document.querySelector("#abs-bcn-map");html2canvas(a).then(t=>{let e=new jsPDF("p","mm","a4");e.addImage(t.toDataURL(),"PNG",0,0,211,298),e.save(a.dataset.filename)})}$(document).ready(function(){$("#exportMap").on("click",function(){printMap()})});
