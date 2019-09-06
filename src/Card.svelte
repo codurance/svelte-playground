9c6bc735976060a3dd56be61647ebb9b109d45a0
Damián Pumar <damianpumar@gmail.com>
2019-09-06 23:05:40 +0200

    Created Card component

---
+++ src/Card.svelte
@@ -0,0 +1,37 @@
+<script>
+  import Card, {
+    Content,
+    PrimaryAction,
+    Media,
+    MediaContent,
+    Actions,
+    ActionButtons,
+    ActionIcons
+  } from "@smui/card";
+  import Button, { Label } from "@smui/button";
+  import IconButton, { Icon } from "@smui/icon-button";
+</script>
+
+<Card
+  style="width: 80%; margin: 0 auto; margin-top: 20px; margin-bottom: 20px;">
+  <Content class="mdc-typography--body2">
+    <slot />
+  </Content>
+  <Actions>
+    <ActionButtons>
+      <Button>
+        <Label>Filter</Label>
+      </Button>
+    </ActionButtons>
+    <ActionIcons>
+      <IconButton toggle aria-label="Add to favorites" title="Add to favorites">
+        <Icon class="material-icons" on>favorite</Icon>
+        <Icon class="material-icons">favorite_border</Icon>
+      </IconButton>
+      <IconButton class="material-icons" title="Share">share</IconButton>
+      <IconButton class="material-icons" title="File download">
+        file_download
+      </IconButton>
+    </ActionIcons>
+  </Actions>
+</Card>
--- src/Charts.svelte
+++ src/Charts.svelte
@@ -1,7 +1,13 @@
-<MapChart />
-<BarChart />
-
 <script>
-import MapChart from './MapChart.svelte';
-import BarChart from './BarChart.svelte';
+  import Card from "./Card.svelte";
+  import BarcelonaMapChart from "./charts/BarcelonaMapChart.svelte";
+  import BarcelonaBarChart from "./charts/BarcelonaBarChart.svelte";
 </script>
+
+<Card>
+  <BarcelonaMapChart />
+</Card>
+<br />
+<Card>
+  <BarcelonaBarChart />
+</Card>
