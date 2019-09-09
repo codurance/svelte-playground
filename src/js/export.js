e8101e8969a03e8fafe6e5719e7de0594e17ed4c
Damián Pumar <damianpumar@gmail.com>
2019-09-09 12:23:10 +0200

    Fixed pdf

--- src/js/export.js
+++ src/js/export.js
@@ -1,11 +1,15 @@
 function printMap() {
   var map = document.querySelector('#abs-bcn-map');
 
-  html2canvas(map).then(canvas => {
-    let pdf = new jsPDF('p', 'mm', 'a4');
-    pdf.addImage(canvas.toDataURL(), 'PNG', 0, 0, 211, 298);
-    pdf.save(map.dataset.filename);
-  });
+  html2canvas(map, { width: 1800, height: 1800, scale: 1 }).then(
+    canvas => {
+      let pdf = jsPDF('p', 'pt', 'a4', true);
+
+      pdf.addImage(canvas.toDataURL(), 'PNG', 5, 22, 920, 1200);
+
+      pdf.save(map.dataset.filename);
+    },
+  );
 }
 
 $(document).ready(function() {
