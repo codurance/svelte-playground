854e111b3fede1dc0381d051cdbed3f06ed97f91
Gabriele Lippi <gabriele@lippi.net>
2019-09-30 16:57:56 +0200

    Add footer component

---
+++ src/Footer.svelte
@@ -0,0 +1,22 @@
+<footer style="display: flex; justify-content: space-around; background-color: #e1ddd5; padding: 8px 12px;">
+  <div style="display: flex; align-items: center">
+    <a href="https://www.aspb.cat">
+      <img src="https://www.aspb.cat/wp-content/themes/aspb/images/logo_pequeno.png">
+    </a>
+
+    <div class="certifcation-wrapper">
+      <a href="https://www.aspb.cat/wp-content/uploads/2019/09/SegellEFQM400_ASPB_2019.pdf" target="_blank">
+	<img src="https://www.aspb.cat/wp-content/themes/aspb/images/certificat-1.png">
+      </a>
+      <a style="width:auto;margin-right: 20px;" target="_blank" href="https://www.aspb.cat/wp-content/uploads/2019/07/Certificat_ISO9001_ASPB.pdf">
+	<img src="https://www.aspb.cat/wp-content/uploads/2019/09/aspb_certificat_iso9001_2015.png">
+      </a>
+      <a style="width:auto;margin-right: 20px;" target="_blank" href="https://www.aspb.cat/wp-content/uploads/2019/07/Certificat_OHSAS18001_ASPB.pdf">
+	<img src="https://www.aspb.cat/wp-content/uploads/2019/09/aspb_certificat_ohsas_18001_2007.png">
+      </a>
+      <a style="width:auto;margin-right: 20px;" href="https://www.aspb.cat/wp-content/uploads/2016/10/CERT_227_LE_1338_aigues.pdf">
+	<img src="https://www.aspb.cat/wp-content/uploads/2019/09/aspb_certificat_iso_17025.png">
+      </a>
+    </div>
+  </div>
+</footer>
